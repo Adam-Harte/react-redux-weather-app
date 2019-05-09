eaf1686bb0ae49f687944b48a661b17673e7ba33
Adam-Harte <adamharte11@hotmail.co.uk>
2019-05-09 23:25:45 +0100

    WeatherDisplay component created to display all the weather data combining the icon and info components

---
+++ src/components/Weather/WeatherDisplay/WeatherDisplay.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import './WeatherDisplay.css';
+
+import WeatherInfo from '../WeatherInfo/WeatherInfo';
+import WeatherIcon from '../WeatherIcon/WeatherIcon';
+
+interface Props {
+  data: any;
+  metric: string;
+}
+
+const WeatherDisplay: React.FC<Props> = props => {
+  let temp = props.data.main.temp;
+
+  if (props.metric === 'celsius') {
+    temp = (props.data.main.temp - 273.15).toFixed(1);
+  } else {
+    temp = (((props.data.main.temp - 273.15) * 9) / 5 + 32).toFixed(1);
+  }
+  return (
+    <div className="WeatherDisplay">
+      <div className="info-container">
+        <WeatherInfo
+          description={props.data.weather[0].description}
+          temp={temp}
+          wind={props.data.wind.speed}
+          humidity={props.data.main.humidity}
+        />
+      </div>
+      <div className="icon-container">
+        <WeatherIcon
+          icon={props.data.weather[0].icon}
+          alt={props.data.weather[0].main}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
